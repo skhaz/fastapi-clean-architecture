260636cc831a0bb4cac6f66f5bfdc91ebbc8bae1
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-02-10 09:55:36 -0300

    Add more tests

--- tests/repositories/test_memory.py
+++ tests/repositories/test_memory.py
@@ -28,10 +28,28 @@ class DummyEntity(BaseEntity):
         }
 
 
-def test_memory_repository_get_single():
+def test_memory_repository_add_and_list():
     repo = MemoryRepository()
     entity = DummyEntity(id=1)
     repo.add(entity)
     entities = repo.list()
 
     assert entities == [entity]
+
+
+def test_memory_repository_remove():
+    repo = MemoryRepository()
+    entity = DummyEntity(id=1)
+    repo.add(entity)
+    result = repo.remove(entity.id)
+
+    assert result
+
+
+def test_memory_repository_get():
+    repo = MemoryRepository()
+    entity = DummyEntity(id=1)
+    repo.add(entity)
+    other = repo.get(entity.id)
+
+    assert other == entity
