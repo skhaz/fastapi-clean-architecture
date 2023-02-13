ded68ec074608b8ac500ecbc54d56ef85bc805e5
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-02-13 13:11:26 -0300

    Fix test

--- tests/use_cases/test_users.py
+++ tests/use_cases/test_users.py
@@ -4,16 +4,20 @@ from app.use_cases.users import UserAddUseCase
 
 
 def test_user_add_use_case():
+    data = {
+        "name": "test",
+        "email": "me@test.com",
+        "avatar": "https://s3.amazon.com/avatar.jpg",
+    }
+
     repo = MemoryRepository()
     case = UserAddUseCase(repo)
-    user = UserRequest(
-        name="test",
-        email="me@test.com",
-        avatar="https://s3.amazon.com/avatar.jpg",
-    )
+    user = UserRequest(**data)
 
-    data = case.execute(user).to_dict()
+    result = case.execute(user).to_dict()
 
-    assert data["name"] == user.name
-    assert data["email"] == user.email
-    assert data["avatar"] == user.avatar
+    assert len(result) == 4
+    assert result["id"] == "1"
+    assert result["name"] == data["name"]
+    assert result["email"] == data["email"]
+    assert result["avatar"] == data["avatar"]
